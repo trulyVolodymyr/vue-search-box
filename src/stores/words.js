45d73ec78d50c6ad74e9ec9658ce5ef34d71cbbf
vladimir vychikov <vovaorc@MacBook-Pro-Vychikov.local>
2022-09-16 18:17:04 +0300

    second

--- src/stores/words.js
+++ src/stores/words.js
@@ -30,16 +30,28 @@ export const useWords = defineStore('words', {
     },
     generateWords() {
       for (let k = 0; k < 50000; k++) {
+        //1 OPTION
+        //
+        //
+        // let result = Math.random().toString(36).slice(2, 7)
+        //2 OPTION
+        //
+        //
         let result = ''
         const characters = 'abcdefghijklmnopqrstuvwxyz'
         let charactersLength = characters.length
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 8; i++) {
           result += characters.charAt(
             Math.floor(Math.random() * charactersLength)
           )
         }
         this.words.push(result)
       }
+      // 3 OPTION
+      //
+      // this.words = this.words = Array.from({ length: 50000 }, () =>
+      //   Math.random().toString(36).slice(2, 9)
+      // )
     },
     setWord() {
       this.enteredWord = this.advisedWord
