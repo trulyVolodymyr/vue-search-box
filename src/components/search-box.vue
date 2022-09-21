c9660e59bcb024a92f6e06fdec5cd11fb0fae45f
vladimir vychikov <vovaorc@MacBook-Pro-Vychikov.local>
2022-09-21 10:25:00 +0300

    press f

--- src/components/search-box.vue
+++ src/components/search-box.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div @keydown.down="state.moveDown" @keydown.up="state.moveUp">
     <form class="form" @submit.prevent>
       <input
         class="input"
@@ -7,26 +7,66 @@
         list="words"
         v-model="state.enteredWord"
         @keydown.enter="state.setWord"
+        @keydown.down="onArrowDown"
+        @keydown.up="onArrowUp"
+        @keydown.delete="state.deleteSymbol"
       />
       <div class="advise">
         {{ state.advisedWord }}
       </div>
-      <datalist id="words">
-        <option
-          v-for="word in state.filteredWords"
-          :key="word"
-          :value="word"
-        ></option>
-      </datalist>
+      <div v-show="state.hidelist" class="options">
+        <ul class="options__list" ref="scrollContainer">
+          <li
+            ref="options"
+            v-for="(word, index) in state.filteredWords"
+            :key="word"
+            class="options__item"
+            @mouseover="state.selected = index"
+            @mouseout="state.selected = false"
+            :class="{ selected: state.selected === index }"
+            @click="state.enteredWord = word"
+          >
+            {{ word }}
+          </li>
+        </ul>
+      </div>
     </form>
   </div>
 </template>
 
 <script setup>
-import { onMounted, onUpdated } from 'vue'
+import { onMounted, onUpdated, ref } from 'vue'
 import { useWords } from '../stores/words'
 const state = useWords()
 
-onMounted(() => state.generateWords())
-onUpdated(() => state.filterWords())
+const scrollContainer = ref(null)
+const options = ref(null)
+
+const fixScrolling = () => {
+  const liH = options.value[state.arrowCounter].clientHeight
+  scrollContainer.value.scrollTop = liH * state.arrowCounter
+}
+
+const onArrowDown = () => {
+  if (state.arrowCounter < state.filteredWords.length - 1) {
+    state.arrowCounter = state.arrowCounter + 1
+    fixScrolling()
+  }
+}
+
+const onArrowUp = () => {
+  if (state.arrowCounter > 0) {
+    state.arrowCounter = state.arrowCounter - 1
+    fixScrolling()
+  }
+}
+
+onMounted(() => {
+  state.generateWords()
+})
+onUpdated(() => {
+  state.filterWords()
+  state.updateSelected()
+  state.updateArrowCounter()
+})
 </script>
--- src/stores/words.js
+++ src/stores/words.js
@@ -6,33 +6,89 @@ export const useWords = defineStore('words', {
       words: [],
       advisedWord: '',
       enteredWord: '',
+      selected: false,
+      arrowCounter: -1,
     }
   },
 
   getters: {
+    hoveredWord() {
+      return this.filteredWords[this.selected]
+    },
+    hidelist: (state) => {
+      if (state.enteredWord === state.filteredWords[0]) {
+        state.selected = false
+        return false
+      } else {
+        return true
+      }
+    },
     filteredWords: (state) => state.filterWords(),
   },
 
   actions: {
+    updateSelected() {
+      if (this.enteredWord === '') {
+        this.selected = false
+      }
+    },
+    updateArrowCounter() {
+      if (this.enteredWord === '') {
+        this.arrowCounter = -1
+      }
+    },
     filterWords() {
       let res = this.words.filter((word) => {
         if (
           word.startsWith(this.enteredWord.trim()) &&
-          this.enteredWord != ''
+          this.enteredWord.trim() != ''
         ) {
           return word
         }
       })
-      this.advisedWord = res[0]
+      if (!this.selected) {
+        this.advisedWord = res[0]
+      }
+
       return res
+    },
+    moveDown() {
+      if (this.selected === false) {
+        return (this.selected = 0)
+      }
+      if (
+        this.selected !== false &&
+        this.selected < this.filteredWords.length - 1
+      ) {
+        return this.selected++
+      }
+    },
+    moveUp() {
+      if (this.selected === false) {
+        return (this.selected = 0)
+      }
+      if (this.selected !== false && this.selected > 0) {
+        return this.selected--
+      }
     },
     generateWords() {
       this.words = this.words = Array.from({ length: 50000 }, () =>
-        Math.random().toString(36).slice(2, 11)
+        Math.random().toString(36).slice(2, 9)
       )
     },
+    deleteSymbol() {
+      this.selected = false
+      this.arrowCounter = -1
+    },
     setWord() {
-      this.enteredWord = this.advisedWord
+      if (!this.selected) {
+        this.arrowCounter = -1
+        this.enteredWord = this.advisedWord
+      }
+      if (this.selected) {
+        this.arrowCounter = -1
+        this.enteredWord = this.hoveredWord
+      }
     },
   },
 })
